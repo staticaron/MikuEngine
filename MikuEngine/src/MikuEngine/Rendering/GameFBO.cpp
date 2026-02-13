7bc6d5447e7a6447f81753e4d01140e92c312d39
staticaron <devm19102003@gmail.com>
2026-02-13 15:51:03 +0530

    FIX: Wrapping of FBO Texture

--- MikuEngine/src/MikuEngine/Rendering/GameFBO.cpp
+++ MikuEngine/src/MikuEngine/Rendering/GameFBO.cpp
@@ -20,6 +20,10 @@ namespace MikuEngine
 
 		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
 		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+
 		glBindTexture( GL_TEXTURE_2D, 0 );
 
 		glFramebufferTexture2D( GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_TextureID, 0 );
