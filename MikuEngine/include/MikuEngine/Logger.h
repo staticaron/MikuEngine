7fa0629b9d0a9b0e09317a79c240696131a21d62
staticaron <devm19102003@gmail.com>
2026-02-21 03:38:16 +0530

    FIX: Removed LINE and FILE from error log

--- MikuEngine/include/MikuEngine/Logger.h
+++ MikuEngine/include/MikuEngine/Logger.h
@@ -37,11 +37,9 @@ namespace MikuEngine
 #define MIKU_CLIENT_WARN( ... )	 MikuEngine::Logger::GetClientLogger()->warn( __VA_ARGS__ )
 #define MIKU_CLIENT_ERROR( ... ) MikuEngine::Logger::GetClientLogger()->error( __VA_ARGS__ )
 
-#define MIKU_CORE_INFO( ... ) MikuEngine::Logger::GetCoreLogger()->info( __VA_ARGS__ )
-#define MIKU_CORE_WARN( ... ) MikuEngine::Logger::GetCoreLogger()->warn( __VA_ARGS__ )
-#define MIKU_CORE_ERROR( ... )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         \
-	MikuEngine::Logger::GetCoreLogger()->error( __VA_ARGS__ );                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     \
-	std::cout << "@ " << __FILE__ << ":" << __LINE__
+#define MIKU_CORE_INFO( ... )  MikuEngine::Logger::GetCoreLogger()->info( __VA_ARGS__ )
+#define MIKU_CORE_WARN( ... )  MikuEngine::Logger::GetCoreLogger()->warn( __VA_ARGS__ )
+#define MIKU_CORE_ERROR( ... ) MikuEngine::Logger::GetCoreLogger()->error( __VA_ARGS__ );
 
 #else
 
