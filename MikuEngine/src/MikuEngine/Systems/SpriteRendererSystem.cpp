2704f3a9792dbad6290f876f173eb28904654869
staticaron <devm19102003@gmail.com>
2026-03-11 04:16:08 +0530

    ADD: Drag Drop Support to Sprite Renderer Component

--- MikuEngine/src/MikuEngine/Systems/SpriteRendererSystem.cpp
+++ MikuEngine/src/MikuEngine/Systems/SpriteRendererSystem.cpp
@@ -78,6 +78,19 @@ namespace MikuEngine
 			ImGui::SameLine();
 			if ( ImGui::Button( "EDIT...##texture" ) ) textureEditBtnCallback();
 
+			if ( ImGui::BeginDragDropTarget() )
+			{
+				auto payload = ImGui::AcceptDragDropPayload( "TEXTURE_DRAG_DROP_PAYLOAD" );
+
+				if ( payload != nullptr )
+				{
+					auto texturePath = static_cast<const char*>( payload->Data );
+					spriteRendererC.TextureIdentifier = textureManager.GetTextureByFilePath( texturePath )->GetUUID();
+				}
+
+				ImGui::EndDragDropTarget();
+			}
+
 			auto shaderUUID = spriteRendererC.ShaderUUID;
 
 			auto shader = shaderUUID.has_value() ? shaderManager.GetShader( shaderUUID.value() ) : shaderManager.GetDefaultShader();
@@ -86,6 +99,19 @@ namespace MikuEngine
 			DISABLED_IMGUI( ImGui::Button( shaderName.c_str() ) );
 			ImGui::SameLine();
 			if ( ImGui::Button( "EDIT...##shader" ) ) shaderEditBtnCallback();
+
+			if ( ImGui::BeginDragDropTarget() )
+			{
+				auto payload = ImGui::AcceptDragDropPayload( "SHADER_DRAG_DROP_PAYLOAD" );
+
+				if ( payload != nullptr )
+				{
+					auto shaderPath = static_cast<const char*>( payload->Data );
+					spriteRendererC.ShaderUUID = shaderManager.GetShaderByFilePath( shaderPath ).shader.GetUUID();
+				}
+
+				ImGui::EndDragDropTarget();
+			}
 
 			ImGui::DragFloat4( "Tint", &spriteRendererC.Tint.x );
 		};
