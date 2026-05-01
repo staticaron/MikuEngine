67b33f73a52a749b04acdc8f2741697963e8942e
staticaron <devm19102003@gmail.com>
2026-05-01 00:13:51 +0000

    ADD: Default Texture Tab View when selecting textures

--- MikuEditor/src/Windows/TextureSelectionWindow.cpp
+++ MikuEditor/src/Windows/TextureSelectionWindow.cpp
@@ -12,29 +12,70 @@ namespace MikuEditor
 
 		ImGui::Begin( "Select Texture", &m_IsOpen );
 
-		auto canvasSize = ImGui::GetContentRegionAvail();
-		auto columns = static_cast<unsigned int>( canvasSize.x / m_ImageSize );
-
-		// Render each texture as image button in table
-		ImGui::BeginTable( "Texture Button Grid", columns );
-
-		auto allTextures = appLevelStuff.GetAssetPoolManager().GetTextureManager().GetAllLoadedTextures();
-
-		for ( auto [ uuid, textureContainer ] : allTextures )
+		if ( ImGui::BeginTabBar( "Textures" ) )
 		{
-			ImGui::TableNextColumn();
-			ImGui::PushID( textureContainer.texture.GetUUID() );
-
-			if ( ImGui::ImageButton( "##TextureBtn", ( void* )( intptr_t )textureContainer.texture.GetRendererID(), ImVec2( 100, 100 ), ImVec2( 1, 1 ), ImVec2( 0, 0 ) ) )
+			if ( ImGui::BeginTabItem( "Project" ) )
 			{
-				m_OnTextureSelection( uuid );
-				response = WindowResponse::COMPLETED;
+				auto canvasSize = ImGui::GetContentRegionAvail();
+				auto columns = static_cast<unsigned int>( canvasSize.x / m_ImageSize );
+
+				if ( columns >= 1 )
+				{
+					// Render each texture as image button in table
+					ImGui::BeginTable( "Texture Button Grid", columns );
+
+					auto allTextures = appLevelStuff.GetAssetPoolManager().GetTextureManager().GetAllLoadedTextures();
+
+					for ( auto [ uuid, textureContainer ] : allTextures )
+					{
+						ImGui::TableNextColumn();
+						ImGui::PushID( textureContainer.texture.GetUUID() );
+
+						if ( ImGui::ImageButton( "##TextureBtn", ( void* )( intptr_t )textureContainer.texture.GetRendererID(), ImVec2( 100, 100 ), ImVec2( 1, 1 ), ImVec2( 0, 0 ) ) )
+						{
+							m_OnTextureSelection( uuid );
+							response = WindowResponse::COMPLETED;
+						}
+
+						ImGui::PopID();
+					}
+
+					ImGui::EndTable();
+				}
+
+				ImGui::EndTabItem();
 			}
 
-			ImGui::PopID();
-		}
+			if ( ImGui::BeginTabItem( "Default" ) )
+			{
+				auto canvasSize = ImGui::GetContentRegionAvail();
+				auto columns = static_cast<unsigned int>( canvasSize.x / m_ImageSize );
 
-		ImGui::EndTable();
+				// Render each texture as image button in table
+				ImGui::BeginTable( "Texture Button Grid", columns );
+
+				auto allTextures = appLevelStuff.GetAssetPoolManager().GetTextureManager().GetAllDefaultTextures();
+
+				for ( auto [ uuid, textureContainer ] : allTextures )
+				{
+					ImGui::TableNextColumn();
+					ImGui::PushID( textureContainer.texture.GetUUID() );
+					if ( ImGui::ImageButton( "##TextureBtn", ( void* )( intptr_t )textureContainer.texture.GetRendererID(), ImVec2( 100, 100 ), ImVec2( 1, 1 ), ImVec2( 0, 0 ) ) )
+					{
+						m_OnTextureSelection( uuid );
+						response = WindowResponse::COMPLETED;
+					}
+
+					ImGui::PopID();
+				}
+
+				ImGui::EndTable();
+
+				ImGui::EndTabItem();
+			}
+
+			ImGui::EndTabBar();
+		}
 
 		ImGui::End();
 
