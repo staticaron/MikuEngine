59bcece7accdfd4c73849e07c53bedf8c5711d9e
staticaron <devm19102003@gmail.com>
2026-05-15 01:03:08 +0100

    FIX: Removed Unnecessary Logging

--- MikuEngine/src/MikuEngine/Rendering/Cubemap.cpp
+++ MikuEngine/src/MikuEngine/Rendering/Cubemap.cpp
@@ -3,8 +3,6 @@
 #include "glad/glad.h"
 #include "stb_image/stb_image.h"
 
-#include "Logger.h"
-
 namespace MikuEngine
 {
 	void Cubemap::LoadFromFile( const std::filesystem::path& path )
@@ -18,8 +16,6 @@ namespace MikuEngine
 			auto newPath = path.parent_path() / ( fileName.string() + std::to_string( x ) + path.extension().string() );
 
 			unsigned char* data = stbi_load( newPath.c_str(), &m_Width, &m_Height, &m_Channels, 4 );
-
-			MIKU_CORE_INFO( "Image Location : {}", newPath.c_str() );
 
 			glTexImage2D( GL_TEXTURE_CUBE_MAP_POSITIVE_X + x, 0, GL_RGBA, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data );
 
