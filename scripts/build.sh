4015358aee12192ecc45132b11a80c534000b56d
staticaron <devm19102003@gmail.com>
2026-05-28 11:25:06 +0100

    FIX: Force using clang instead of gcc

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=Debug
+cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 
 cmake --build build
 
